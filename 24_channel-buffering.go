f7fcb858808f7eca079283acbb7ee11a63665a73
Clare Glinka <clare.glinka@returnpath.com>
2015-10-16 15:41:29 -0500

    Trying to send two messages to an unbuffered channel gets you a fatal error: all goroutines area asleep - deadlock

--- 24_channel-buffering.go
+++ 24_channel-buffering.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	messages := make(chan string, 2)
+	nonbuff := make(chan string)
 
 	messages <- "buffered"
 	messages <- "channel"
+
+	nonbuff <- "a test"
+	nonbuff <- "refused"
 
 	fmt.Println(<-messages)
 	fmt.Println(<-messages)
